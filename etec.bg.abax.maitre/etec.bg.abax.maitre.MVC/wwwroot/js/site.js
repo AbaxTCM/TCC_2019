08b93de6d72cba35e446dc28316b0bedd26c2bfb
GiseleAlves <gialmeidaetec899@gmail.com>
2019-05-12 23:36:15 -0300

    Inserido login e cadastro

--- etec.bg.abax.maitre/etec.bg.abax.maitre.MVC/wwwroot/js/site.js
+++ etec.bg.abax.maitre/etec.bg.abax.maitre.MVC/wwwroot/js/site.js
@@ -5,26 +5,15 @@
 
 
 $(document).ready(function () {
-    var contador = 1;
-    var img1 = "login1.jpg";
-    var img2 = "img2.jpg";
-    var img3 = "img3.jpg";
-    var img4 = "img4.png";
-    var img5 = "img5.jpg";
-    var img6 = "img6.jpg";
 
-    Exibindo();
-
-    function Exibindo() {
-        if (!document.images)
-            return;
-
-        document.images["slide"].src = eval("img" + contador);
-
-        if (contador < 6)
-            contador++;
-        else
-            contador = 1;
-        setTimeout("Exibindo()", 3000);
 });
+function validar() {
+    var email, senha;
+    email = document.form.email.value;
+    senha = document.form.senha.value;
+    if ((email == "") && (senha == "")) {
+        alert("Por favor preencher este campo");
+    }
+
+}
 
